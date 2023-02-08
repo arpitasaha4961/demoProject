ee383d1b6ceb01422360869a9e424f7226c5f5ef
arpitasaha4961 <arpita4961@gmail.com>
2023-02-08 17:46:13 +0600

    created course table

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   validates :title, :course_code, :credit, presence: true
   validates :title, :course_code, uniqueness: true
   validates :credit, numericality: {greater_than_or_equal_to: 1}
+  has_and_belongs_to_many :student_profiles
 end
--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,2 +1,4 @@
 class StudentProfile < ApplicationRecord
+  validates :student_id, :name, :cgpa, presence: :true
+  has_and_belongs_to_many :courses
 end
